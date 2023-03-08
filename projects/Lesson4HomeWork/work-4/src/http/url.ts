b178017283ee0313c50065471ae3f5e1520c1f01
saozcan <samimozcan@outlook.com>
2023-03-09 01:16:16 +0300

    work-4 is not finished, I'll try at morning. But the other ones done even work-8

---
+++ projects/Lesson4HomeWork/work-4/src/http/url.ts
@@ -0,0 +1,17 @@
+const websiteUrl = "http://www.atez.com.tr:8080/about"
+const websiteUrlEN = "http://www.atez.com.tr:8080/en/about"
+
+const urlParams = url.parse(websiteUrl);
+const urlParamsEN = url.parse(websiteUrlEN);
+console.log(urlParams);
+console.log('EN', urlParamsEN);
+
+/**
+ * Ödev parse yaparak hangi dilde yapıldığını bulunacak
+ * en de ru ae
+ *
+ * "http://www.atez.com.tr:8080/en/about"
+ * "http://www.atez.com.tr:8080/ru/about" hangi dili destekliyor ?
+ */
+
+
